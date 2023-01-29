ce50930fde3883e2a792104cd8e986999e5fd198
fbhood <mail@fabiopacifici.com>
2023-01-29 12:30:48 +0100

    ad abuot route

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/about', function () {
+    return 'About Me';
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
